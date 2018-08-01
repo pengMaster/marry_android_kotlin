3fa7c5c70e3302dae8bfcb99b4dccb832c7d5724
wp521 <946549990@qq.com>
2018-08-01 10:31:40 +0800

    init

---
+++ app/src/main/java/king/bird/marrykotlin/act/MainActivity.kt
@@ -0,0 +1,13 @@
+package king.bird.marrykotlin.act
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import king.bird.marrykotlin.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
